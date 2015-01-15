4138d33d3166b97829295915b97be09306d58870
gcool-info <george.koulouris1@gmail.com>
2015-01-15 22:36:53 +0000

    Added scrolling to top

--- router.js
+++ router.js
@@ -8,26 +8,42 @@ var Router = Backbone.Router.extend({
 	},
 
 	home: function() {
+		// Scroll to the top
+		$("html, body").animate({ scrollTop: 0 }, "slow");
+
 		var home = new homeView({model: model});
 		home.render();
 	},
 
 	viewVideo: function() {
+		// Scroll to the top
+		$("html, body").animate({ scrollTop: 0 }, "slow");
+
 		var video = new videoView({model: model});
 		video.render();
 	},
 
 	editInfo: function() {
+		// Scroll to the top
+		$("html, body").animate({ scrollTop: 0 }, "slow");
+
 		var editPage = new editView({model: model});
 		editPage.render();
 	},
 
 	viewDetails: function() {
+
+		// Scroll to the top
+		$("html, body").animate({ scrollTop: 0 }, "slow");
+
 		var detailsPage = new detailsView({model: model});
 		detailsPage.render();
 	},
 
 	participateForm: function() {
+		// Scroll to the top
+		$("html, body").animate({ scrollTop: 0 }, "slow");
+		
 		var participatePage = new participateView({model: model});
 		participatePage.render();
 	}
